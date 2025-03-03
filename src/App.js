2e7825e376556cb772552c458c2d8a9f85e4a627
vohai1011 <142618703+vovanhai2005@users.noreply.github.com>
2025-03-03 15:17:45 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.css'; // Import file CSS
 
 function App() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/data.json');
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        const data = await response.json();
+        setBooks(data);
+      } catch (error) {
+        console.error('Error fetching data: ', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h1>Quản Lý Thư Viện</h1>
       </header>
+      <table className="book-table">
+        <thead>
+          <tr>
+            <th>Tên sách</th>
+            <th>Tác giả</th>
+            <th>Tình trạng</th>
+            <th>Số lượng</th>
+            <th>Thể loại</th>
+          </tr>
+        </thead>
+        <tbody>
+          {books.map((book, index) => (
+            <tr key={index}>
+              <td>{book.tên_sách}</td>
+              <td>{book.tác_giả}</td>
+              <td>{book.tình_trạng}</td>
+              <td>{book.số_lượng}</td>
+              <td>{book.thể_loại}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
