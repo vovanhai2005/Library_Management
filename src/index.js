750ebb31df4e5fd83553077770f8afd8fb2a98e7
vohai1011 <142618703+vovanhai2005@users.noreply.github.com>
2025-03-12 23:54:07 +0700

    Add previous version repo

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // Thay đổi cách import
+import App from './App';
+
+// Tạo root và render ứng dụng
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
